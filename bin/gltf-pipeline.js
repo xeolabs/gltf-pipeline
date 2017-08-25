3ce3aec82f45183393081c19c09263315d4c8fed
Sean Lilley <lilleyse@gmail.com>
2017-08-25 16:48:14 -0400

    Temp

--- bin/gltf-pipeline.js
+++ bin/gltf-pipeline.js
@@ -1,5 +1,22 @@
-// #!/usr/bin/env node
-// 'use strict';
+#!/usr/bin/env node
+'use strict';
+//
+//
+// var args = process.argv;
+// args = args.slice(2, args.length);
+// var options = parseArguments(args);
+//
+//
+// // console.time('optimize');
+// // Node automatically waits for all promises to terminate
+// processFileToDisk(options.inputPath, options.outputPath, options)
+//     .then(function() {
+//         console.timeEnd('optimize');
+//     });
+//
+//
+//
+// var path = require('path');
 // var args = process.argv;
 // var gltfPath = args[2];
 // var glbPath = gltfPath.slice(0, -5) + '.glb';
@@ -9,21 +26,62 @@
 //
 // var gltfContents = fsExtra.readFileSync(gltfPath);
 // var gltf = JSON.parse(gltfContents);
-// gltfToGlb(gltf)
+// var options = {
+//     basePath : path.dirname(gltfPath)
+// };
+// gltfToGlb(gltf, options)
 //     .then(function(glb) {
 //         fsExtra.outputFileSync(glbPath, glb);
 //     });
+//
+// 'use strict';
+// var args = process.argv;
+// var glbPath = args[2];
+// var gltfPath = glbPath.slice(0, -4) + '.gltf';
+//
+// var fsExtra = require('fs-extra');
+// var glbToGltf = require('../lib/glbToGltf');
+//
+// var glb = fsExtra.readFileSync(glbPath);
+// glbToGltf(glb)
+//     .then(function(gltf) {
+//         fsExtra.outputJsonSync(gltfPath, gltf);
+//     });
 
-'use strict';
+// var args = process.argv;
+// var glbPath = args[2];
+// var glbOptimizedPath = glbPath.slice(0, -4) + '-optimized.glb';
+//
+// var fsExtra = require('fs-extra');
+// var glbToGltf = require('../lib/glbToGltf');
+// var gltfToGlb = require('../lib/gltfToGlb');
+//
+// var glb = fsExtra.readFileSync(glbPath);
+// glbToGltf(glb)
+//     .then(function(gltf) {
+//         return gltfToGlb(gltf)
+//             .then(function(glb) {
+//                 fsExtra.outputFileSync(glbOptimizedPath, glb);
+//             });
+//     });
+
 var args = process.argv;
-var glbPath = args[2];
-var gltfPath = glbPath.slice(0, -4) + '.gltf';
+var gltfPath = args[2];
+var gltfOptimizedPath = gltfPath.slice(0, -5) + '-optimized.gltf';
 
+var path = require('path');
 var fsExtra = require('fs-extra');
-var glbToGltf = require('../lib/glbToGltf');
+var processGltf = require('../lib/processGltf');
 
-var glb = fsExtra.readFileSync(glbPath);
-glbToGltf(glb)
+var gltf = fsExtra.readJsonSync(gltfPath);
+var options = {
+    basePath : path.dirname(gltfOptimizedPath),
+    separateBuffers : true,
+    separateTextures : true,
+    separateShaders : true
+};
+
+processGltf(gltf, options)
     .then(function(gltf) {
-        fsExtra.outputJsonSync(gltfPath, gltf);
+        fsExtra.outputJsonSync(gltfOptimizedPath, gltf);
     });
